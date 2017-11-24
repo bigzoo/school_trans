f8d46fbdb5afee9ca3892b30a871c9efcf7d52d8
Chris Nyaga <nyagachris75@gmail.com>
2017-11-24 12:17:01 +0300

    Created a mailing template to be used when sending invitation emails.

---
+++ resources/views/emails/invite_school.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>You are Invited</title>
+</head>
+<body>
+<h1>Your School has been invited.</h1>
+<p>
+    Your magic word is {{ $school->magic_word }}
+</p>
+<p>
+    Keep it secret!, you'll need it to set a password.
+</p>
+</body>
+</html>
