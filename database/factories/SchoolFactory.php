ca9589b76652ee7ba65f7e325c7416727150cbba
Chris Nyaga <nyagachris75@gmail.com>
2017-11-24 15:55:26 +0300

    Created a factory for creating schools in a seeder.

---
+++ database/factories/SchoolFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\School;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\School::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'magic_word' => School::magic_word()
+    ];
+});
